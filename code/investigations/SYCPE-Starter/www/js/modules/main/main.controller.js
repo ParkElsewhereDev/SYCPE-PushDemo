24ce391eea4726309e5ce881f0268a5a5ba13379
Dave Mee <davemee@gmail.com>
2018-12-17 19:31:54 +0000

    Built SYCPE mobile app and browser QR code show-er

---
+++ code/investigations/SYCPE-Starter/www/js/modules/main/main.controller.js
@@ -0,0 +1,94 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('main' )
+    .controller('mainCtrl', mainCtrl);
+
+  mainCtrl.$inject = [
+    '$ionicPlatform',
+    '$scope',
+    '$state',
+    '$sce',
+    '$http',
+    'pushSrvc',
+    'uuid'
+  ];
+2
+  function mainCtrl(
+    $ionicPlatform,
+    $scope,
+    $state,
+    $sce,
+    $http,
+    pushSrvc,
+    uuid
+  ) {
+
+    var vm=angular.extend(this, {
+
+    });
+
+	  vm.MESSAGE_TIMEOUT_SECONDS = 10;
+
+    vm.pushConnected = false;
+
+    vm.inbound = { data: { },
+                   rendered: "No messages yet." };
+
+    vm.subscriptionFeedback = "";
+
+
+    vm.initialise = function initialise() {
+
+      vm.inbound.rendered = "No registrationId yet...";
+
+      pushSrvc.initialisePush( function deviceNowConnected( data ){
+        console.log("controller initialised push, got payload ",data );
+        vm.inbound.rendered = "Got connected payload";
+        if (data.hasOwnProperty('registrationId')===true) {
+
+          vm.registrationId = data.registrationId;
+          vm.pushConnected = true;
+
+          pushSrvc.setCallback( vm.handleInbound );
+          pushSrvc.setTimeout( vm.MESSAGE_TIMEOUT_SECONDS * 1000 );
+        }
+      });
+    };
+
+    vm.startCodeScan = function startCodeScan() {
+      console.log("starting a QR code scan");
+      cordova.plugins.barcodeScanner.scan(
+        function(qrResult) { // .text .format .cancelled
+          console.log("scanned",qrResult.text);
+          if(qrResult.cancelled===true) {
+            console.log("aborted scan!");
+            return;
+          } else {
+            if(qrResult.format==="QR_CODE") {
+              pushSrvc.subscribe( qrResult.text );
+            }
+          }
+        },
+        function(error) {
+          console.log("error scanning",error);
+        },
+        {
+          showTorchButton: true,
+          saveHistory: false,
+          prompt: "Scan the QR Code"
+        }
+      );
+    };
+
+    vm.handleInbound = function handleInbound( data ) {
+      console.log("got inbound message", data);
+      alert(data);
+
+    };
+
+    vm.initialise();
+
+  }
+})();
